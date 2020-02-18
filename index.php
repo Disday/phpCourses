4be29fe95b25013685a4d86c79992ef31719a92b
Temin Dmitrii <disdain44@yandex.ru>
2020-02-19 02:06:47 +0300

    12

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-print_r($_SERVER); 
-echo "<br>";
-print_r($_GET);
+// print_r();
+switch ($_SERVER['REQUEST_URI']) {
+  case '/':
+    echo '<a href="/welcome">welcome</a><br><a href="/not-found">not-found</a>';
+    break;
+  case '/welcome':
+    echo '<a href="/">main</a>';
+    break;
+  default:
+    header('HTTP/1.0 404 Not Found');
+    echo 'Page not found. <a href="/">main</a>';
+}
