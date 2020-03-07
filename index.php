7575368e4828d2a74f704bddfcbec3548339c768
Disday <disdain44@yandex.ru>
2020-03-07 19:34:02 +0300

    1

--- index.php
+++ index.php
@@ -1,33 +1,18 @@
 <?php
-function make(){
-  return [];
+function sayPrimeOrNot($num)
+{
+  $result = primeOrNot($num) ? 'yes' : 'no';
+  print_r($result);
 }
-function set(&$map, $key, $value){
-  $hash = crc32($key) % 1000;
-  // print_r("{$hash} \n");
-  if( !empty($map[$hash]) && $map[$hash][0] !== $key ){
-  // echo 'error', "\n";
-  return false;
+function primeOrNot($num)
+{
+  if($num <= 1) return false;
+  for ($i = 2; $i < $num; $i += ($num % $i)) {
+    echo $i, ' ', "\n";
+    if ($num % $i === 0) {
+      return false;
+    }
   }
-  $map[$hash] = [$key, $value];
   return true;
 }
-
-function get($map, $key, $default = null){
-  $hash = crc32($key) % 1000;
-  if (isset($map[$hash]) && $map[$hash][0] === $key) {
-    return $map[$hash][1];
-  }
-  return $default;
-}
-
-$map = make();
-
-set($map, 'aaaaa0.462031558722291', 'vvv');
-set($map, 'aaaaa0.0585754039730588', 'boom!');
-
-print_r($map);
- $result = get($map, 'aaaaa0.462031558722291');
-var_dump($result);
- $result = get($map, 'aaaaa0.0585754039730588');
-var_dump($result);
+sayPrimeOrNot(11);
