a4273ccada77f76b54843ad8640aef8d94918e12
Temin Dmitrii <disdain44@yandex.ru>
2020-03-13 21:34:55 +0300

    1

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 function union($first, ...$rest)
 {
   $merged = array_merge($first, ...$rest);
@@ -8,11 +7,3 @@ function union($first, ...$rest)
   return $result;
 }
 var_dump(union([3, 2], [2, 2, 1])); // ['a', 3, false, true, 5, 8]
-=======
-// use Funct\Collection;
-
-$last = function(string $str){
-    return strlen($str) < 1 ? null : $str[strlen($str) - 1];
-};
-var_dump($last(''));
->>>>>>> 8b2cbfd3a8172806553b1a47c914dc20ab6bc796
