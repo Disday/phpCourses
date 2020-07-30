9088acd2687209057a061c041572362e8a248781
Disday <disdain44@yandex.ru>
2020-07-30 23:55:22 +0300

    1

--- src/Poly/1.php
+++ src/Poly/1.php
@@ -4,16 +4,4 @@ namespace Poly\TicTacToe;
 // use Toe;
 require __DIR__ . '/../../vendor/autoload.php';
 
-function flatten($arr)
-{
-    $res = [];
-    foreach ($arr as $elem) {
-        if (is_array($elem)) {
-            $res = [...$res, ...$elem];
-        } else $res[] = $elem;
-    }
-    return $res;
-}
-
-// flatten([1, [3, 2], 9]); // [1, 3, 2, 9]
-flatten([1, [[2], [3]], [9]]); // [1, [2], [3], 9]
+var_dump((base64_decode('frrh5tbmjp4adl05lm24cobj03')));
