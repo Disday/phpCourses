05abfdd733da31b286e2122e3d54c3ec4d0f7c40
Disday <disdain44@yandex.ru>
2020-09-29 01:05:07 +0300

    test

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-function reverse($number)
-{
-    return $number > 0 ? (int)strrev(abs($number)) : (int) -strrev(abs($number));
-}
-var_dump(reverse('123456'));
+$str = 'Something to bear in mind is that regex is actually a declarative programming language like prolog : your regex is a set of rules which the regex interpreter tries to match against a string.   During this matching, the interpreter will assume certain things, and continue assuming them until it comes up against a failure to match, which then causes it to backtrack.  Regex assumes "greedy matching" unless explicitly told not to, which can cause a lot of backtracking.  A general rule of thumb is that the more backtracking, the slower the matching process';
+$pattern = "/in[dg]/";
+print_r(preg_filter($pattern, 'HERE', $str));
+
