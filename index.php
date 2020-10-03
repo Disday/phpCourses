5ecdd5e633108b36350f896ac0434735fd3cf55e
Temin Dmitrii <disdain44@yandex.ru>
2020-10-03 16:47:31 +0300

    1

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-$links = [
-  ['url' => 'https://google.com', 'name' => 'Google'],
-  ['url' => 'https://yandex.com', 'name' => 'Yandex'],
-  ['url' => 'https://bingo.com', 'name' => 'Bingo']
-];
-foreach ($links as $link) : ?>
-  <div><a href=<?= $link['url'] ?>> <?= $link['name'] ?></a></div>
-<?php endforeach; ?>
+    const strings = ['(one) ($%@#$) (value1)', 'test (^,ehu-) ) (t) (//)',
+      '2383', '()', '() ('];
+      // $pattern = '/^\w+@[a-zA-Z]{3,}\.[a-zA-Z]{2,5}$/';
+      $pattern = '/\(.+\)/';
+  print_r(preg_grep($pattern, strings));
---
+++ src/Poly/TicTacToe/TicTacToe.php
@@ -0,0 +1,11 @@
+<?php
+namespace Poly;
+require_once  __DIR__ . '/../../../vendor/autoload.php';
+
+class TicTacToe
+{
+  public function __construct($difficulty = 'easy')
+  {
+    
+  }
+}
